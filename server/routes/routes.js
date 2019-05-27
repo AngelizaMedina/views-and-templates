97e755b7d5127b1af6a29fc32734e4c7e3e0c52d
Angeliza Medina Quintana <5884603@rts-365.dk>
2019-05-27 13:06:12 +0200

    Stuff

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,13 +1,21 @@
 module.exports = (app) => {
+
    app.get('/', (req, res, next) => {
-      res.render('home', {'title': 'Fancy Homepage'}); //Dette indlæser ejs filen
+      res.render('home', {'title': 'Home - Skullcandy'}); //This loads the .ejs document
+      console.log("Something"); //Gets logged in our terminal
    });
 
    app.get('/product', (req, res, next) => {
-      res.render('product');
+      res.render('product', {'title': 'Product - Skullcandy'});
    });
    
    app.get('/contact', (req, res, next) => {
-      res.render('contact');
+      res.render('contact', {'title': 'Contact - Skullcandy'});
    });
-};
+
+   app.get('/test', (req, res, next) => {
+      console.log("Test");
+      res.send();
+   });
+
+}; //module.exports end
