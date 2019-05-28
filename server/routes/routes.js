4056ab8027a17f09ba3aa8e16f80842193912cac
Angeliza Medina Quintana <5884603@rts-365.dk>
2019-05-28 09:30:22 +0200

    Add a class dynamically + Add stuff and CSS to the home page

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,16 +1,26 @@
 module.exports = (app) => {
 
    app.get('/', (req, res, next) => {
-      res.render('home', {'title': 'Home - Skullcandy'}); //This loads the .ejs document
+      res.render('home', {
+         'title': 'Home - Skullcandy',
+         'page': 'home'
+      }); //This loads the .ejs document
+
       console.log("Something"); //Gets logged in our terminal
    });
 
    app.get('/product', (req, res, next) => {
-      res.render('product', {'title': 'Product - Skullcandy'});
+      res.render('product', {
+         'title': 'Product - Skullcandy',
+         'page': 'product'
+      });
    });
    
    app.get('/contact', (req, res, next) => {
-      res.render('contact', {'title': 'Contact - Skullcandy'});
+      res.render('contact', {
+         'title': 'Contact - Skullcandy',
+         'page': 'contact'
+      });
    });
 
    app.get('/test', (req, res, next) => {
